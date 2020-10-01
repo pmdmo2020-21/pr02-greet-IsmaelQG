4def6150441f6882022f27abe1f21a31799fb368
Ismael <ismaeljesusquiros@gmail.com>
2020-10-01 18:46:19 +0200

    Parte RadioButton y RadioGroup hecha en java

--- app/src/main/java/es/iessaladillo/pedrojoya/greet/MainActivity.java
+++ app/src/main/java/es/iessaladillo/pedrojoya/greet/MainActivity.java
@@ -1,15 +1,46 @@
 package es.iessaladillo.pedrojoya.greet;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import es.iessaladillo.pedrojoya.greet.databinding.MainActivityBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private MainActivityBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        binding = MainActivityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        image();
+    }
+
+    private void image(){
+        binding.rdbTreatmentMr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.imgTreatment.setImageResource(R.drawable.ic_mr);
+            }
+        });
+
+        binding.rdbTreatmentMrs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.imgTreatment.setImageResource(R.drawable.ic_mrs);
+            }
+        });
+
+        binding.rdbTreatmentMs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.imgTreatment.setImageResource(R.drawable.ic_ms);
+            }
+        });
+
     }
 
 }
